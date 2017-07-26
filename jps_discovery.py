6957334db2859da47636645680e62efaa3836d5d
viethdgit <uocthem1dieu@gmail.com>
2017-07-26 17:15:57 +0700

    Add files via upload

--- jps_discovery.py
+++ jps_discovery.py
@@ -2,7 +2,6 @@
 import os
 import json
 if __name__=="__main__":
-		os.system('sudo rm -rf /tmp/hsperfdata_*')
         process_id=[]
         process_id=str(os.popen("sudo jps | awk '{if (($2 != \"Jps\")&&($2 != \"stat\")) print $1}'").read()).rstrip().split('\n')
         data = [{"{#PID}": pid} for pid in process_id]
