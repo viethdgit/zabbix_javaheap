8521e77a7359e1e490807276ea45b46188e887b7
viethdgit <uocthem1dieu@gmail.com>
2017-07-28 11:59:28 +0700

    Add files via upload

--- jps_discovery.py
+++ jps_discovery.py
@@ -2,6 +2,6 @@
 import os
 import json
 if __name__=="__main__":
-	java_process=str(os.popen("sudo jps | grep -v 'Jps' | grep -v 'Jstat'").read()).rstrip().split('\n'):
+	java_process=str(os.popen("sudo jps | grep -v 'Jps' | grep -v 'Jstat'").read()).rstrip().split('\n')
 	data = [{"{#PID}": pid} for pid in java_process]
 	print(json.dumps({"data": data}, indent=4))
